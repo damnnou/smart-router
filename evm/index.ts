537cdec14a5e2e81a93d00e82e85788dc11f6a96
damnnou <skdamn@outlook.com>
2024-05-06 22:12:14 +0300

    init: smart-router

---
+++ evm/index.ts
@@ -0,0 +1,4 @@
+export * from './constants'
+export { feeOnTransferDetectorAddresses, fetchTokenFeeOnTransfer, fetchTokenFeeOnTransferBatch } from './fot'
+export * from './v3-router'
+export * from './v3-router/types'
